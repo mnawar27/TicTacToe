ac89d36575f232911356cb1a6fb344e34d95c74b
Maisha Nawar <mnawar@Zipcoders-MacBook-Pro.local>
2024-11-07 22:05:41 -0500

    created two more methods that checks the winner and tells if board is full or not

--- src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
+++ src/main/java/rocks/zipcodewilmington/tictactoe/Board.java
@@ -4,23 +4,64 @@ package rocks.zipcodewilmington.tictactoe;
  * @author leon on 6/22/18.
  */
 public class Board {
+    private Character[][] matrix;
+
     public Board(Character[][] matrix) {
+        this.matrix = matrix;
     }
 
+    private Boolean checkWinner(char player) {
+        for (int i = 0; i < 3; i++) {
+            if ((matrix[i][0] == player && matrix[i][1] == player && matrix[i][2] == player) ||
+                    (matrix[0][i] == player && matrix[1][i] == player && matrix[2][i] == player)) {
+                return true;
+            }
+        }
+
+        if ((matrix[0][0] == player && matrix[1][1] == player && matrix[2][2] == player) ||
+                (matrix[0][2] == player && matrix[1][1] == player && matrix[2][0] == player)) {
+            return true;
+        }
+
+        return false;
+    }
+
+
     public Boolean isInFavorOfX() {
-        return null;
+
+        return checkWinner('X');
     }
 
     public Boolean isInFavorOfO() {
-        return null;
+
+        return checkWinner('O');
+    }
+
+    private Boolean isBoardFull() {
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                if (matrix[i][j] == null) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 
     public Boolean isTie() {
-        return null;
+
+        return !isInFavorOfX() && !isInFavorOfO() && isBoardFull();
     }
 
     public String getWinner() {
-        return null;
+
+        if (isInFavorOfX()) {
+            return "X";
+        } else if (isInFavorOfO()) {
+            return "O";
+        } else {
+            return "";
+        }
     }
 
 }
